6e4e1bf98acb7e0a1956f9f5d43d71d52277b784
AliReza <ali.reza.buet@gmail.com>
2023-07-26 19:03:53 +0600

    Complete Task 1

--- strLen.js
+++ strLen.js
@@ -1,5 +1,10 @@
 const strLen = (str) => {
-    return str.length
-}
-
-module.exports = strLen
+    if (str.length < 1 || str.length > 10) {
+      throw new Error('Input string length should be between 1 and 10 characters.');
+    }
+  
+    return str.length;
+  };
+  
+  module.exports = strLen;
+  
--- stringLength.test.js
+++ stringLength.test.js
@@ -1,7 +1,13 @@
-// stringLength.test.js
 const strLen = require('./strLen');
 
 test('returns the length of a non-empty string', () => {
-  expect(strLen('Hello, world!')).toBe(13);
+  expect(strLen('Findthefoo')).toBe(10);
 });
 
+test('throws an error if input is an empty string', () => {
+  expect(() => strLen('')).toThrow('Input string length should be between 1 and 10 characters.');
+});
+
+test('throws an error if input string length is greater than 10 characters', () => {
+  expect(() => strLen('This is too long.')).toThrow('Input string length should be between 1 and 10 characters.');
+});
